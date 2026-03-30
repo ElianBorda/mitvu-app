f917deb4087113a753e1d62f6e4aefa5cabddb4d
ElianBorda <eliancamiloalejandro@gmail.com>
2026-03-30 09:02:58 -0300

    first commit

---
+++ src/test/java/com/unq/mitvu/MitvuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unq.mitvu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitvuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
