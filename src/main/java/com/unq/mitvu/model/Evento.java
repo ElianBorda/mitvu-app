ef08e32b9b2c895320cf5d7a36c385176fc4cb0d
ElianBorda <eliancamiloalejandro@gmail.com>
2026-04-28 19:29:22 -0300

    add: se agregan los atributos a la clase Evento

--- src/main/java/com/unq/mitvu/model/Evento.java
+++ src/main/java/com/unq/mitvu/model/Evento.java
@@ -1,4 +1,13 @@
 package com.unq.mitvu.model;
 
+import java.time.LocalDateTime;
+
 public class Evento {
+    private String id;
+    private String titulo;
+    private String descripcion;
+    private LocalDateTime fecha;
+    private String idComision; // Si es null, es un evento GLOBAL
+    private String creadoPorId; // ID del Tutor o Admin que lo creó
+    private boolean esGlobal;
 }
