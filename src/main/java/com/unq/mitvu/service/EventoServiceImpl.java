2af478560f40ee23000a6698e9bf4363f63972eb
ElianBorda <eliancamiloalejandro@gmail.com>
2026-04-28 19:57:12 -0300

    add: se agregan los metodos a implementar de EventoService

--- src/main/java/com/unq/mitvu/service/EventoServiceImpl.java
+++ src/main/java/com/unq/mitvu/service/EventoServiceImpl.java
@@ -1,4 +1,42 @@
 package com.unq.mitvu.service;
 
-public class EventoServiceImpl {
+import com.unq.mitvu.model.Evento;
+
+import java.util.List;
+
+public class EventoServiceImpl implements EventoService{
+    @Override
+    public Evento crear(Evento evento) {
+        return null;
+    }
+
+    @Override
+    public List<Evento> crearTodos(List<Evento> eventos) {
+        return List.of();
+    }
+
+    @Override
+    public Evento obtenerPorId(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Evento> obtenerTodos() {
+        return List.of();
+    }
+
+    @Override
+    public Evento modificarPorId(String id, Evento evento) {
+        return null;
+    }
+
+    @Override
+    public void eliminarPorId(String id) {
+
+    }
+
+    @Override
+    public void eliminarTodo() {
+
+    }
 }
