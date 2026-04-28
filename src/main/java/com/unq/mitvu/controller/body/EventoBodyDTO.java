0f6c39bc0b2663fdfd47b77d97d122955132990c
ElianBorda <eliancamiloalejandro@gmail.com>
2026-04-28 19:39:49 -0300

    add: se agregan los atributos a EventoBodyDTO

--- src/main/java/com/unq/mitvu/controller/body/EventoBodyDTO.java
+++ src/main/java/com/unq/mitvu/controller/body/EventoBodyDTO.java
@@ -1,5 +1,12 @@
 package com.unq.mitvu.controller.body;
 
 
+import java.time.LocalDateTime;
+
 public class EventoBodyDTO {
+    private String titulo;
+    private String descripcion;
+    private LocalDateTime fecha;
+    private String idComision;
+    private boolean generarParaTodos = false;
 }
