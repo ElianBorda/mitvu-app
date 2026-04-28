d0183beb75dbd7ea340becbc84ca5e0721b9376c
ElianBorda <eliancamiloalejandro@gmail.com>
2026-04-28 19:50:56 -0300

    add: se crea la consulta findByComisionOGlobal

--- src/main/java/com/unq/mitvu/dao/EventoDAO.java
+++ src/main/java/com/unq/mitvu/dao/EventoDAO.java
@@ -2,8 +2,14 @@ package com.unq.mitvu.dao;
 
 import com.unq.mitvu.model.Evento;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EventoDAO extends MongoRepository<Evento, String> {
+
+    @Query("{ $or: [ { 'idComision': ?0 }, { 'esGlobal': true } ] }")
+    List<Evento> findByComisionOCentral(String idComision);
 }
