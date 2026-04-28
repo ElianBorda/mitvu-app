eeafd533731d6adae9caa37bee4793f5e9439703
ElianBorda <eliancamiloalejandro@gmail.com>
2026-04-28 19:32:38 -0300

    add: se agrega la annotation JsonFormat a la fecha del evento

--- src/main/java/com/unq/mitvu/model/Evento.java
+++ src/main/java/com/unq/mitvu/model/Evento.java
@@ -1,11 +1,14 @@
 package com.unq.mitvu.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 
 public class Evento {
     private String id;
     private String titulo;
     private String descripcion;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm")
     private LocalDateTime fecha;
     private String idComision; // Si es null, es un evento GLOBAL
     private String creadoPorId; // ID del Tutor o Admin que lo creó
