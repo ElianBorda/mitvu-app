04cdbc3f86d77ba3f30d80434cc2692520f6cc89
ElianBorda <eliancamiloalejandro@gmail.com>
2026-04-28 19:49:46 -0300

    add: EventoDAO extiende de MongoRepository

--- src/main/java/com/unq/mitvu/dao/EventoDAO.java
+++ src/main/java/com/unq/mitvu/dao/EventoDAO.java
@@ -1,7 +1,9 @@
 package com.unq.mitvu.dao;
 
+import com.unq.mitvu.model.Evento;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventoDAO {
+public interface EventoDAO extends MongoRepository<Evento, String> {
 }
