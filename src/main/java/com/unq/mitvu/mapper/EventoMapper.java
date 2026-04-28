3ed8497bb7723313725e6a60250eadfedfe15231
ElianBorda <eliancamiloalejandro@gmail.com>
2026-04-28 20:08:09 -0300

    add: se implementó EventoMapper

--- src/main/java/com/unq/mitvu/mapper/EventoMapper.java
+++ src/main/java/com/unq/mitvu/mapper/EventoMapper.java
@@ -1,4 +1,11 @@
 package com.unq.mitvu.mapper;
 
+import com.unq.mitvu.model.Evento;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface EventoMapper {
+    void actualizarEvento(Evento datosNuevos, @MappingTarget Evento entidadExistente);
 }
