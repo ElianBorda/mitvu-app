cb6a4e05dd59cf343f970dbe3bd23110fc3fd2b6
ElianBorda <eliancamiloalejandro@gmail.com>
2026-04-28 19:55:59 -0300

    add: se crean los metodos iniciales del CRUD de Evento

--- src/main/java/com/unq/mitvu/service/EventoService.java
+++ src/main/java/com/unq/mitvu/service/EventoService.java
@@ -1,4 +1,17 @@
 package com.unq.mitvu.service;
 
+import com.unq.mitvu.model.Evento;
+import jdk.jfr.Event;
+
+import java.util.List;
+
 public interface EventoService {
+
+    Evento crear(Evento evento);
+    List<Evento> crearTodos(List<Evento> eventos);
+    Evento obtenerPorId(String id);
+    List<Evento> obtenerTodos();
+    Evento modificarPorId(String id, Evento evento);
+    void eliminarPorId(String id);
+    void eliminarTodo();
 }
