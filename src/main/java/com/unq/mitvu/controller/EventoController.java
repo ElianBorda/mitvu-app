8bd8ab8039b31ca25a1bf7d3efbdc1876d5fd4d4
ElianBorda <eliancamiloalejandro@gmail.com>
2026-04-28 21:10:11 -0300

    add: se creó y se implementó obtenerEventos en EventoController

--- src/main/java/com/unq/mitvu/controller/EventoController.java
+++ src/main/java/com/unq/mitvu/controller/EventoController.java
@@ -9,10 +9,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/eventos")
@@ -25,10 +24,15 @@ public class EventoController {
 
     @PostMapping
     public ResponseEntity<Evento> crearEventoAdmin(@Valid @RequestBody EventoBodyDTO eventoBodyDTO) {
-
         Evento evento = eventoMapper.aEvento(eventoBodyDTO);
-        Evento nuevoEvento = eventoService.crearParaCalendario(evento, new Usuario());
+        Evento nuevoEvento = eventoService.crear(evento);
         return new ResponseEntity<>(nuevoEvento, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Evento>> obtenerEventos() {
+        List<Evento> eventos = eventoService.obtenerTodos();
+        return new ResponseEntity<>(eventos, HttpStatus.OK);
     }
 
 }
