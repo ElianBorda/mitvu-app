7d0d23306bc0ac42353b6d3b2d5e22db2a77cb3d
ElianBorda <eliancamiloalejandro@gmail.com>
2026-04-28 21:33:27 -0300

    add: se agrega el setUp y el tearDown

--- src/test/java/com/unq/mitvu/service/EventoServiceTest.java
+++ src/test/java/com/unq/mitvu/service/EventoServiceTest.java
@@ -1,4 +1,45 @@
 package com.unq.mitvu.service;
 
-public class EventoServiceTest {
+import com.unq.mitvu.dao.EventoDAO;
+import com.unq.mitvu.model.Evento;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class EventoServiceTest {
+
+    @Autowired
+    private EventoService eventoService;
+
+    @Autowired
+    private EventoDAO eventoDAO;
+
+    private Evento eventoPrueba;
+
+    @BeforeEach
+    void setUp() {
+        eventoDAO.deleteAll();
+        eventoPrueba = new Evento();
+        eventoPrueba.setTitulo("Tutoría de Programación");
+        eventoPrueba.setDescripcion("Repaso para el parcial");
+        eventoPrueba.setFecha(LocalDateTime.of(2026, 4, 28, 15, 30));
+    }
+
+    @AfterEach
+    void tearDown() {
+        eventoDAO.deleteAll();
+    }
+
+
 }
